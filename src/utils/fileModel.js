9b657c58d3b8fce16119002e11b8b30cdf7a4761
JackChuChou <3212497319@qq.com>
2019-08-08 10:26:03 +0800

    update: err resolve

--- src/utils/fileModel.js
+++ src/utils/fileModel.js
@@ -4,13 +4,19 @@ const path = require('path')
 function writeFile(info) {
   const fileName = path.resolve(__dirname, '../../db/', info.fileName + '-' + info.fileMD5)
   if (fs.existsSync(fileName)) {
-    console.log('file already exists!')
+    // console.log('file already exists!')
+    console.log(`[UTILS]: file already exists`);
     return
   }
   return new Promise(function(resolve, reject) {
     fs.writeFile(fileName, info.data, function(err) {
-    if (err) throw err
-    console.log('write file Done!')
+    // if (err) throw err
+    if (err) {
+        console.log(`[ERROR]: ${err}`);
+        reject();
+    }
+    // console.log('write file Done!')
+    console.log(`[UTILS]: write file done`);
     resolve()
   })
   })
